b4970b0d2ddb43bfe1bb54978bbc09f4b4e2359d
Vitoco <vc.acunauribe@gmail.com>
2021-03-20 16:58:39 -0300

    End of section 6

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +12,7 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor( public accountService: AccountService ) { }
+  constructor( public accountService: AccountService, private router: Router, private toastr: ToastrService ) { }
 
   ngOnInit(): void { }
 
@@ -18,14 +20,18 @@ export class NavComponent implements OnInit {
     this.accountService.login( this.model )
       .subscribe(
         response => {
-          console.log( response );
+          this.router.navigateByUrl('/members');
         },
-        error => console.error( error )
+        error => {
+          console.error( error );
+          this.toastr.error(error.error);
+        }
       );
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -12,7 +13,7 @@ export class RegisterComponent implements OnInit {
 
   model: any = {};
 
-  constructor( private accountService: AccountService ) { }
+  constructor( private accountService: AccountService, private toastr: ToastrService ) { }
 
   ngOnInit(): void { }
 
@@ -23,12 +24,14 @@ export class RegisterComponent implements OnInit {
           console.log( response );
           this.cancel();
         },
-        error => console.error( error )
+        error => {
+          console.error( error );
+          this.toastr.error(error.error);
+        }
       );
   }
 
   cancel() {
     this.cancelRegister.emit( false );
   }
-
 }
